56ab9b5d990bed3ec52047834223244c3ba0c98e
hossein masoumi <hossein.masoumi1382@gmail.com>
2026-06-14 15:18:28 +0330

    fix: route dashboard through chart wrapper

--- Online_monitoring_of_quality_control/urls.py
+++ Online_monitoring_of_quality_control/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from core.views import HomeView
+from core.dashboard_fixes import FixedDashboardView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
+    path('dashboard/', FixedDashboardView.as_view(), name='dashboard'),
     path('', include('core.urls')),
     path("project/",include("project.urls")),
     path('auth/experiment/', include('experiment.urls', namespace='experiment')),
